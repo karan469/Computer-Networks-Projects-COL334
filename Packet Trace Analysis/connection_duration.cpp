77b0b7d0a9ccdc2b82243d50bcc165077e4f4275
Karan Tanwar <33158202+CRekkaran@users.noreply.github.com>
2019-10-13 20:05:18 +0530

    Update connection_duration.cpp

--- Packet Trace Analysis/connection_duration.cpp
+++ Packet Trace Analysis/connection_duration.cpp
@@ -29,14 +29,89 @@ vector<string> giveTokens(string s){
 	return res;
 }
 
+int synOrAck(string s){ //RETURN: -1: SYN | 0: SYN,ACK | 1: ACK
+	string s1 = s;
+	string s2 = "[SYN]";
+	string s3 = "[SYN,ACK]";
+	string s4 = "[ACK]";
+	if (s1.find(s2) != std::string::npos){
+		return -1;
+	} else if (s1.find(s3) != std::string::npos){
+		return 0;
+	} else if (s1.find(s4) != std::string::npos){
+		return 1;
+	}
+	return INT_MAX;
+}
+
+int synOrFinAckOrRst(string s){
+	string s1 = s;
+	string s2 = "[SYN]";
+	string s3 = "[FIN, ACK]";
+	string s4 = "[RST]";
+	if (s1.find(s2) != std::string::npos){
+		return -1;
+	} else if (s1.find(s3) != std::string::npos){
+		return 0;
+	} else if(s1.find(s4) != std::string::npos){
+		return 1;
+	}
+	return INT_MAX;
+}
+
+string sourceip(string s){
+	return giveTokens(s)[2];
+}
+
+string destip(string s){
+	return giveTokens(s)[3];
+}
+
+void printSpecialMaps(map<pair<string, string>, pair<int, int>> res){
+	map<pair<string, string>, pair<int, int>>::iterator itr;
+	int count = 0;
+	for(itr = res.begin();itr!=res.end();itr++){
+		// pair<string, string> tp = itr->first;
+		// cout<<tp.first;
+		count++;
+		/*if((itr->second).first ==0)*/ cout<<(itr->first).first << " - " << (itr->first).second << " | " << (itr->second).first << " - " << (itr->second).second<<endl;
+		// if(count>20) break;
+	}
+	// cout<<"Total Connections: "<<count<<endl;
+}
+
 void connectionDuration(string filename){
+	map<pair<string, string>, pair<int, int>> res;
+	// res.insert({make_pair("0","0"), make_pair(0,0)});
+
 	ifstream myfile(filename);
 	if(myfile.is_open() && filename.find("_letsee_all") != std::string::npos){
 		// cout<<"OPENED " + filename<<endl;
-		
+		string l;
+		while(getline(myfile,l)){
+			vector<string> line = giveTokens(l);
+			string sourceIP = sourceip(l);
+			string destIP = destip(l);
+			int its_time = stoi(line[1].substr(1,line[1].length()-2));
+			// cout<<sourceIP<< " ? " <<destIP<<endl;
+			pair<string, string> pr1 = make_pair(sourceIP, destIP);
+			pair<string, string> pr2 = make_pair(destIP, sourceIP);
+			map<pair<string, string>, pair<int, int>>::iterator it = res.find(pr1);
+			map<pair<string, string>, pair<int, int>>::iterator it2 = res.find(pr2);
+			
+			if(it==res.end() && synOrFinAckOrRst(line[6])==-1){
+				res.insert({pr1, make_pair(its_time,0)});
+			} else if((it2!=res.end()) && synOrFinAckOrRst(line[6])>=0){
+				it2->second.second = its_time;// = make_pair(it->second.first, its_time);
+			} else if((it!=res.end()) && synOrFinAckOrRst(line[6])>=0){
+				it->second.second = its_time;// = make_pair(it->second.first, its_time);
+			}
+		}
+		printSpecialMaps(res);
 	} else {
 		cout<<"File " + filename + " not opened correctly."<<endl;
 	}
+
 }
 
 int main(int argc, char const *argv[])
