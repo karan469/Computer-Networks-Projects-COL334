5f682282318091d3192a203b4aa7dd7c3b6dd3a8
Karan Tanwar <33158202+CRekkaran@users.noreply.github.com>
2019-10-15 01:28:35 +0530

    Update script.sh

--- Packet Trace Analysis/script.sh
+++ Packet Trace Analysis/script.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if test ! -d ./outputs; then mkdir ./outputs; fi
+if test ! -d ./outputs/q1; then mkdir ./outputs/q1; fi
+if test ! -d ./outputs/q2; then mkdir ./outputs/q2; fi
+if test ! -d ./outputs/q3; then mkdir ./outputs/q3; fi
+if test ! -d ./outputs/q4; then mkdir ./outputs/q4; fi
+if test ! -d ./outputs/q5; then mkdir ./outputs/q5; fi
+if test ! -d ./outputs/q6; then mkdir ./outputs/q6; fi
+if test ! -d ./outputs/q7; then mkdir ./outputs/q7; fi
+if test ! -d ./outputs/q8; then mkdir ./outputs/q8; fi
+if test ! -d ./outputs/q9; then mkdir ./outputs/q9; fi
+if test ! -d ./outputs/q10; then mkdir ./outputs/q10; fi
+
 count=`ls -1 *.csv 2>/dev/null | wc -l`
 if [ $count != 0 ]; then rm *.csv;echo Removed residual csv files; fi;
 cp ./apache_logs/*.csv  ./;
